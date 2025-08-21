946a67af3c25e643d2887d98a6b8728d7d3a4482
Md. Khairul Alam Mazumder <khairul@aiub.edu>
2025-08-21 16:08:43 +0600

    new Code

---
+++ view/login.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head></head>
+    <body>
+        <form action="" method="">
+            <label for="userName">user Name</label>
+            <input type="text" id="userName"><br>
+            <label for="pass">Password</label>
+            <input type="password" id="pass"><br>
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
